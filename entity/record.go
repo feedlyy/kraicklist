3b0c7d386a392c5d33a6f068d8394b43511bd482
Fadli <mnurfadd@gmail.com>
2021-04-14 14:42:56 +0700

    added entity record

---
+++ entity/record.go
@@ -0,0 +1,15 @@
+package entity
+
+type Record struct {
+	ID        int64    `json:"id"`
+	Title     string   `json:"title"`
+	Content   string   `json:"content"`
+	ThumbURL  string   `json:"thumb_url"`
+	Tags      []string `json:"tags"`
+	UpdatedAt int64    `json:"updated_at"`
+	ImageURLs []string `json:"image_urls"`
+}
+
+type Searcher struct {
+	Records []Record
+}
