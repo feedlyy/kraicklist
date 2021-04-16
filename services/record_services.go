1e7f21466c44bbbcc0e13807f8e0098d8d7362d9
Fadli <mnurfadd@gmail.com>
2021-04-16 17:59:35 +0700

    update search mechanism

--- services/record_services.go
+++ services/record_services.go
@@ -5,7 +5,6 @@ import (
 	"challenge.haraj.com.sa/kraicklist/repository"
 	"github.com/sahilm/fuzzy"
 	"log"
-	"sort"
 	"strings"
 )
 
@@ -22,8 +21,23 @@ type RecordServ struct {
 	recordScore []entity.RecordWithScore
 }
 
+type Recordss []entity.Record
+
 func NewRecordServices(record repository.RecordRepository) *RecordServ {
 	return &RecordServ{repoRecord: record}
+}
+
+func (e Recordss) String(i int) string {
+	if e[i].Title > e[i].Content {
+		return e[i].Title
+	} else if e[i].Content > e[i].Title {
+		return e[i].Content
+	}
+	return e[i].Title + e[i].Content
+}
+
+func (e Recordss) Len() int {
+	return len(e)
 }
 
 func (s *RecordServ) Search(query string) ([]entity.Record, error) {
@@ -32,56 +46,15 @@ func (s *RecordServ) Search(query string) ([]entity.Record, error) {
 	if err != nil {
 		log.Fatal(err)
 	}
+	fill := append(Recordss{}, records.Records...)
 
-	for _, record := range records.Records {
-		if check[strings.ToLower(query)] {
-			log.Println("Cached successful")
-			return cached[strings.ToLower(query)], nil
+	finds := fuzzy.FindFrom(strings.ToLower(query), fill)
+	for _, r := range finds {
+		if r.Score > 0 && strings.Contains(strings.ToLower(r.Str), strings.ToLower(query)) {
+			result = append(result, fill[r.Index])
+		} else if r.Score > 0 {
+			result = append(result, fill[r.Index])
 		}
-
-		querys := strings.Split(strings.ToLower(query), " ")
-		title := strings.ToLower(record.Title)
-		contentCheck := []string{strings.ToLower(record.Content)}
-		titleCheck := []string{strings.ToLower(record.Title)}
-
-		checkContent := fuzzy.Find(strings.ToLower(query), contentCheck)
-		score := 0
-		for _, fuzzys := range checkContent {
-			score = fuzzys.Score
-		}
-
-		checkTitle := fuzzy.Find(strings.ToLower(query), titleCheck)
-		scoreTitles := 0
-		for _, fuzzys := range checkTitle {
-			scoreTitles = fuzzys.Score
-		}
-
-		/*terms:
-			1. search by exact title
-			2. search by score fuzzy and 'first key word' if it contains more than 1 word
-		       ex: iphone black, then the keyword are iphone*/
-
-		if strings.Contains(title, strings.ToLower(query)) {
-			s.recordScore = append(s.recordScore, entity.RecordWithScore{
-				Record: record,
-				Score:  int64(scoreTitles),
-			})
-		} else if score >= 0 && strings.Contains(title, querys[0]) {
-			s.recordScore = append(s.recordScore, entity.RecordWithScore{
-				Record: record,
-				Score:  int64(score),
-			})
-		}
-	}
-
-	// Sort by it's score
-	sort.SliceStable(s.recordScore, func(i, j int) bool {
-		return s.recordScore[i].Score > s.recordScore[j].Score
-	})
-
-	//loop the record that already sorted
-	for _, value := range s.recordScore {
-		result = append(result, value.Record)
 	}
 
 	cached[strings.ToLower(query)] = result
